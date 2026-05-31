0d63b0792d1498ccb12fc56a9dba3ad866dca531
Dharshan <acer@example.com>
2026-05-31 19:55:41 +0530

    Initial commit

---
+++ python/ex.py
@@ -0,0 +1,17 @@
+# print("Hello",end=" ") #end=" " is used to avoid the default behavior of print which is to move to a new line after printing. By setting end=" ", we are telling print to end with a space instead of a newline, allowing the next print statement to continue on the same line.
+# print("World")
+
+# # variables
+# x=150
+# print(type(x)) #type() is a built-in function in Python that returns the type of the specified object. In this case, it will return <class 'int'>, indicating that x is an integer.
+# y=200   
+# print(x+y)
+
+# x=int(input ("Enter a number: ")) #input() is a built-in function in Python that allows the user to take input from the console. The input is returned as a string, so we use int() to convert it to an integer before storing it in the variable x.
+# y=int(input ("Enter another number: "))
+# print(x+y)
+
+
+
+
+
